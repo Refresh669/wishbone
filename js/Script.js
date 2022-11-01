6bb90a4153811b3c66a45abad81da0d2065bbcf3
refresh669 <vlad27171@gmail.com>
2022-11-01 19:53:27 +0200

    gat

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,21 @@
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:0,
